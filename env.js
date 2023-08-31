54caa96660c1a5864f255e9433e420a015be5d67
= <=>
2023-08-31 15:16:00 +0700

    tambah testing get user

---
+++ env.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+    return {
+      baseUrl: 'https://gorest.co.in/public-api/',
+      authToken: '5d69b4a7dee21a3543d68592a8bdc0769fd71a3fb214b0a3fa27512acefd9d59',
+    };
+  };
