f7711f27f781140d79f32dcd13e711beaa70bb90
= <=>
2023-09-01 10:08:25 +0700

    get id from respone body

---
+++ test/api/user.js
@@ -0,0 +1,85 @@
+const request = require('supertest');
+const {expect} = require('chai');
+
+const server = require('../../env');
+const baseUrl = server().baseUrl;
+const authToken = server().authToken;
+let userId;
+
+describe('Post New User', () => {
+    it('Response Status Equal to 200', async () => {
+        const newUser = {
+            "name": "Salsabila",
+            "email": "salsabila@jast-herman.example",
+            "gender": "female",
+            "status": "inactive"
+        };
+        try {
+            const response = await request(baseUrl)
+              .post(`/users?access-token=${authToken}`)
+              .send(newUser);
+
+            expect(response.status).to.equal(200);
+          } catch (error) {
+            console.error('Error:', error);
+        }
+    });
+})
+
+describe('Post Duplicate User', () => { 
+  it('Response Status Equal to 422', async () => {
+    const newUser = {
+        "name": "Salsabila",
+        "email": "salsabila@jast-herman.example",
+        "gender": "female",
+        "status": "inactive"
+    };
+    try {
+        const response = await request(baseUrl)
+          .post(`/users?access-token=${authToken}`)
+          .send(newUser);
+
+        expect(response.status).to.equal(422);
+      } catch (error) {
+        console.error('Error:', error);
+    }
+  });
+});
+
+describe('Get All User', () => {
+  it('Response Status Equal to 200', async () => {
+    const response = await request(baseUrl)
+      .get(`/users?access-token=${authToken}`)
+      expect(response.status).to.equal(200)
+    })
+});
+
+describe('Get Selected User Details', () =>{  
+  it('Response Status Equal to 200', async () => {
+    const response = await request(baseUrl)
+      .get(`/users/${userId}?access-token=${authToken}`)
+      expect(response.status).to.equal(200)
+      })
+});
+
+describe('Put Selected User Details', () =>{
+  it('Response Status Equal to 200', async () => {
+    const response = await request(baseUrl)
+      .put(`/users/${userId}?access-token=${authToken}`)
+      .send({
+        "name": "Salsabila",
+        "email": "salsabila@jast-herman.example",
+        "gender": "female",
+        "status": "active"
+    });
+      expect(response.status).to.equal(200)
+      })
+});
+
+describe('Delete Selected User', () =>{
+  it('Response Status Equal to 200', async () => {
+      const response = await request(baseUrl)
+      .delete(`/users/${userId}?access-token=${authToken}`)
+      expect(response.status).to.equal(200)
+      })
+});
